cbeb259a1ef0a01eb5824814960f6b9f0716393c
wx <13210240113@fudan.edu.cn>
2014-04-10 01:27:43 +0800

    comnd

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -10,6 +10,14 @@ SparseMatrix::_ColIterator ::_ColIterator(SparseMatrix *_sm,int _col,int _pos)
 	pos = _pos ;
 }
 
+SparseMatrix::_RowIterator ::_RowIterator (SparseMatrix *_sm,int _row,int _pos)
+	:sm(_sm),row(_row)
+{
+	pos = _pos ;
+}
+
+
+
 SparseMatrix::SparseMatrix(vector<TripleNode> & scoreList){
 	//dataOfRowPri
 	int row_id_old;
@@ -24,6 +32,8 @@ SparseMatrix::SparseMatrix(vector<TripleNode> & scoreList){
 		dataOfRowPri[i] = scoreList[i];             //defuat assignment constructor
 		int row_id = scoreList[i].user_id;
 		int col_id = scoreList[i].brand_id;
+		items.insert(col_id);
+		users.insert(row_id);
 		int score = scoreList[i].score;
 		if(row_id != row_id_old)
 		{
@@ -66,15 +76,67 @@ SparseMatrix::SparseMatrix(vector<TripleNode> & scoreList){
 	}
 	colEnd[col_id_old] = scoreList.size() - 1 ;
 	cols = colBegin.size();
+
 	numOfNodes = scoreList.size();
+	sparseRatio = (double)numOfNodes/(cols*rows);
+
 
 }
-TripleNode SparseMatrix::getElement(int i,int j){
+int SparseMatrix::getElement(int i,int j){
 	//int index = rowBegin[i];
 	//return dataOfRowlPri[index];
-	int index = colBegin[j];
-	return dataOfColPri[index];
+
+	TripleNode *it = find_if(&dataOfRowPri[rowBegin[i]],&dataOfRowPri[rowEnd[i]]+1,[=](TripleNode &cur){ return cur.brand_id == j;}) ;
+
+	if(it == &dataOfRowPri[rowEnd[i]] + 1 )
+		{
+			return 0;
+	    }
+	return it->score;
+
+
+
 }
+
+
+int SparseMatrix::innerProduct(int _row1,int _row2)
+{
+	int _innerProduct = 0 ;
+	SparseMatrix::RowIterator it1 = BeginOfRow(_row1);
+	SparseMatrix::RowIterator it2 = BeginOfRow(_row2);
+	while (it1 !=EndOfRow(_row1) && it2!=EndOfRow(_row2))
+	{
+		if((*it1).brand_id == (*it2).brand_id)
+		{
+			_innerProduct += (*it1).score * (*it2).score;
+			it1 ++ ;
+			it2 ++ ;
+		}
+		else if((*it1).brand_id < (*it2).brand_id)
+		{
+			it1 ++ ;
+		}
+		else
+		{
+			it2 ++ ;
+		}
+	}
+	return _innerProduct;
+}
+
+
+
+int SparseMatrix::norm(int _row){
+	int sum_of_elems=0;
+	std::for_each(&dataOfRowPri[rowBegin[_row]],&dataOfRowPri[rowEnd[_row]]+1,
+		[&](TripleNode &t){
+		sum_of_elems += t.score*t.score;  
+	    }
+	);
+   return sum_of_elems;
+}
+
+
 
 SparseMatrix::~SparseMatrix(){
 	delete [] dataOfColPri;
--- SparseMatrix.h
+++ SparseMatrix.h
@@ -1,18 +1,25 @@
+#pragma once
 #include <map>
 #include<vector>
 #include <assert.h>
 #include <iostream>
 #include "DataLoder.h"
 #include <cassert>
+#include <set>
 using namespace XY;
 using std::map;
 using std::vector;
+using std::set;
 namespace wx{
 class SparseMatrix{
-private:
+public:
 	int rows,cols;
 	int numOfNodes;
-	float sparseRatio;
+	double sparseRatio;
+	set<int> items;
+	set<int> users;
+private:
+	
 
 	map<int,int> rowBegin;
 	map<int,int> rowEnd;
@@ -21,8 +28,9 @@ private:
 	map<int,int> colBegin;
 	map<int,int> colEnd;
 	TripleNode *dataOfColPri;
-public:
 
+public:
+	SparseMatrix(vector<TripleNode> & scoreList);         //Constructor
 	
 	//friend RowIterator;  //to do
 	class _ColIterator{
@@ -56,12 +64,12 @@ public:
 		}*/
 	private:
 		int col,pos;
-		SparseMatrix *sm;
+		 SparseMatrix *sm;
 	};
 
 	typedef _ColIterator ColIterator;
-	                                          //typedef complete definition
-	ColIterator BeginOfCol(int col){
+                                       //typedef complete definition
+	ColIterator BeginOfCol(int col)  {
 		/*if(colBegin.find(col)== colBegin.end())
 		{
 		std::cout << " the col is invalid" <<std::endl;
@@ -70,16 +78,63 @@ public:
 		assert(colBegin.find(col)!= colBegin.end());
 		return ColIterator(this,col,colBegin[col]);	
 	};
-	ColIterator EndOfCol(int col){
+	ColIterator EndOfCol(int col) {
 		return ColIterator(this,col,colEnd[col]+1);	
 	};
-	SparseMatrix(vector<TripleNode> & scoreList);
+
+	class _RowIterator{
+	public:
+		_RowIterator(SparseMatrix *_sm,int _Row,int _pos);
+		/*_RowIterator(const _RowIterator &coI){
+			sm = new SparseMatrix(Row);
+		}*/
+		_RowIterator & operator +=(int i)
+		{
+			pos += i;
+			assert(sm->rowBegin[row] <= pos && pos <= sm->rowEnd[row]+1);
+			return *this;
+		}
+		bool operator !=(const _RowIterator & rhs)
+		{
+			return pos!= rhs.pos;		
+		}
+
+		TripleNode * operator ->()
+		{
+			return &sm->dataOfRowPri[pos] ;
+		}
+		_RowIterator operator ++(int)
+		{
+			_RowIterator temp(*this);
+			pos++;
+			return temp;
+		}
+		TripleNode &operator *(){
+			return  sm->dataOfRowPri[pos];
+		}
+		/*TripleNode* operator ->(){
+			return  sm->dataOfRowPri[pos];
+		}*/
+	private:
+		int row,pos;
+	    SparseMatrix *sm;
+	};
+	typedef _RowIterator RowIterator;
+		RowIterator BeginOfRow(int _row) {
+
+		assert(rowBegin.find(_row)!= rowBegin.end());
+		return RowIterator(this,_row,rowBegin[_row]);	
+	};
+	RowIterator EndOfRow(int Row) {
+		return RowIterator(this,Row,rowEnd[Row]+1);	
+	};
+
+	int getElement(int i,int j) ;
 
 
-	float getSparseRatio(){
-		return numOfNodes/cols*rows ; 
-	}
-	TripleNode getElement(int i,int j);
+	int innerProduct(int _row1,int _row2);
+	int norm(int row);
+
 	~SparseMatrix();
 
 };
